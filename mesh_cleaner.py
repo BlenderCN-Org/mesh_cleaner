b84992d26536099ef3f196ec22b558448b092e9e
Greenbaburu <pigiard@gmail.com>
2018-08-14 20:47:27 +0200

    Add clean_all_the_meshes feature
    Add clean_all_the_meshes feature

--- mesh_cleaner.py
+++ mesh_cleaner.py
@@ -9,9 +9,9 @@
 '''
 
 bl_info = {
-    "name": "Mesh Cleaner",
+    "name": "mesh_cleaner",
     "author": "Greenbaburu",
-    "version": (0, 1, 0),
+    "version": (0, 2, 0),
     "blender": (2, 79, 0),
     "location": "View3D > Tools > Mesh Cleaner",
     "description": "Clean the mesh",
@@ -20,12 +20,46 @@ bl_info = {
 
 import bpy
 
+objects_list = []
+
 # remove doubles and dissolve limit
 def redis():
     bpy.ops.mesh.select_all(action='SELECT')           
     bpy.ops.mesh.remove_doubles(threshold=0.0001, use_unselected=False)
     bpy.ops.mesh.dissolve_limited(angle_limit=0.0872665, use_dissolve_boundaries=False, delimit={'NORMAL'})
     bpy.ops.mesh.normals_make_consistent(inside=False)
+
+class AllMeshCleanerOperator(bpy.types.Operator):
+    """Clean all the meshes"""
+    bl_idname = "mesh.mesh_clean_all_operator"
+    bl_label = "Mesh Cleaner Operator"
+    
+    name = bpy.props.StringProperty()
+    
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        name = self.name
+        scene = context.scene
+
+        print(objects_list)        
+           
+        for obj in objects_list:
+            
+            object = scene.objects[obj.name]
+            bpy.context.scene.objects.active = object
+            
+            try: # from Edit Mode
+                redis()
+            except: # from Object Mode 
+                bpy.ops.object.editmode_toggle()
+                redis()
+                bpy.ops.object.editmode_toggle()        
+        
+        return {'FINISHED'}
+       
 
 class MeshCleanerOperator(bpy.types.Operator):
     """Clean mesh"""
@@ -66,12 +100,17 @@ class Mesh_cleaner_panel(bpy.types.Panel):
         
         row_1 = layout.row()
         row_1.label(text="Selected Objects:") 
+                
+        row_cleanAll = layout.row()
+        row_cleanAll.operator("mesh.mesh_clean_all_operator", text="Clean all the meshes", icon="PARTICLEMODE").name # = obj.name
         
-        objects = context.scene.objects
+        objects = bpy.context.scene.objects
     
         for obj in objects:            
-            if obj.select == True and obj.type == 'MESH':
-                
+            if obj.select == True and obj.type == 'MESH':                    
+                    
+                    if obj not in objects_list: objects_list.append(obj)
+                    
                     row_2 = layout.row()                
                     row_2.prop(obj, "name", text="")
                     row_2.prop(obj, "hide", text="")
@@ -80,10 +119,12 @@ class Mesh_cleaner_panel(bpy.types.Panel):
 def register():
     bpy.utils.register_class(Mesh_cleaner_panel)
     bpy.utils.register_class(MeshCleanerOperator)
+    bpy.utils.register_class(AllMeshCleanerOperator)
 
 def unregister():
     bpy.utils.unregister_class(Mesh_cleaner_panel)
     bpy.utils.unregister_class(MeshCleanerOperator)
+    bpy.utils.unregister_class(AllMeshCleanerOperator)
 
 if __name__ == "__main__":
     register()
